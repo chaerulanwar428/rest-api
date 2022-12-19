e854f67f0ad0e1347a0d09956da289daf2ad4e18
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-20 01:32:48 +0700

    mengubah json menjadi object

--- JSON/script.js
+++ JSON/script.js
@@ -1,7 +1,18 @@
-let mahasiswa = {
-  nama: 'Chaerul Anwar',
-  nim: '15200135',
-  email: 'chaerulanwar428@gmail.com',
+// let mahasiswa = {
+//   nama: 'Chaerul Anwar',
+//   nim: '15200135',
+//   email: 'chaerulanwar428@gmail.com',
+// };
+
+// console.log(JSON.stringify(mahasiswa));
+
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    let mahasiswa = JSON.parse(this.responseText);
+    console.log(mahasiswa);
+  }
 };
 
-console.log(JSON.stringify(mahasiswa));
+xhr.open('GET', 'coba.json', true);
+xhr.send();
