910cfb88078a628377b32d93cb2674617f2427d2
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-21 05:41:56 +0700

    mnemapilkan data dari api

---
+++ wpu-movie/js/script.js
@@ -0,0 +1,47 @@
+$('#search-button').on('click', function () {
+  $.ajax({
+    url: 'http://omdbapi.com',
+    type: 'get',
+    dataType: 'json',
+    data: {
+      apikey: '810c6ee5',
+      s: $('#search-input').val(),
+    },
+    success: function (result) {
+      if (result.Response == 'True') {
+        let movies = result.Search;
+
+        $.each(movies, function (i, data) {
+          $('#movielist').append(
+            `
+            <div class="col-md-4">
+            <div class="card mb-3">
+  <img src="` +
+              data.Poster +
+              `" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">` +
+              data.Title +
+              `</h5>
+    <h6 class="card-subtitle mb-2 text-muted">` +
+              data.Year +
+              `</h6>
+              <a href="#" class="card-link">See Detail</a>
+  </div>
+</div>
+</div>`
+          );
+        });
+      } else {
+        $('#movielist').html(
+          `
+        <div class="col">
+        <h1 class="text-center">` +
+            result.Error +
+            `</h1>
+        </div>`
+        );
+      }
+    },
+  });
+});
