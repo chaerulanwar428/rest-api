27ea5017e9d1fa997ce9cccc478d5855aeaec66c
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-21 09:32:47 +0700

    menambahkan fitur see detail

--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -28,7 +28,9 @@ function searchMovie() {
     <h6 class="card-subtitle mb-2 text-muted">` +
               data.Year +
               `</h6>
-              <a href="#" class="card-link">See Detail</a>
+              <a href="#" class="card-link seedetail" data-toggle="modal" data-target="#exampleModal" data-id="` +
+              data.imdbID +
+              `">See Detail</a>
   </div>
 </div>
 </div>`
@@ -56,4 +58,53 @@ $('#search-input').on('keyup', function (e) {
   if (e.which === 13) {
     searchMovie();
   }
+});
+
+$('#movielist').on('click', '.seedetail', function () {
+  $.ajax({
+    url: 'http://omdbapi.com',
+    type: 'get',
+    dataType: 'json',
+    data: {
+      apikey: '810c6ee5',
+      i: $(this).data('id'),
+    },
+    success: function (movie) {
+      if (movie.Response === 'True') {
+        $('.modal-body').html(
+          `
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="` +
+            movie.Poster +
+            `" class="img-fluid">
+                    </div>
+                    
+                    <div class="col-md-8">
+                    <ul class="list-group">
+                    <li class="list-group-item"><h3>` +
+            movie.Title +
+            `</h3></li>
+                    <li class="list-group-item">Released :` +
+            movie.Released +
+            `</li>
+                    <li class="list-group-item">Genre :` +
+            movie.Genre +
+            `</li>
+                    <li class="list-group-item">Director :` +
+            movie.Director +
+            `</li>
+                    <li class="list-group-item">Actors :` +
+            movie.Actors +
+            `</li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+            `
+        );
+      }
+    },
+  });
 });
