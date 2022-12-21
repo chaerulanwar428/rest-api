dc0c9fcc34ac8335099b81090c1663a7978019dd
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-22 04:04:27 +0700

    memnambahkanbeberapa fitur yang tersambung ke api youtube

--- wpu-portfolio/index.php
+++ wpu-portfolio/index.php
@@ -1,3 +1,29 @@
+<?php
+
+function get_CURL($url)
+{
+
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  $result = curl_exec($curl);
+  curl_close($curl);
+  
+  return json_decode($result, true);
+}
+
+$result = get_CURL('https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCkXmLjEr95LVtGuIm3l2dPg&key=AIzaSyA8gvYVMIkZHhN97MXZRraZFPiWElJWAAs');
+$youtubeProfilePic = $result['items'][0]['snippet']['thumbnails']['medium']['url'];
+$chanelName = $result['items'][0]['snippet']['title'];
+$subscriber = $result['items'][0]['statistics']['subscriberCount'];
+
+
+//latest video
+$urlLatestVideo = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyA8gvYVMIkZHhN97MXZRraZFPiWElJWAAs&chanelId=UCkXmLjEr95LVtGuIm3l2dPg&maxResults=1&order=date&part=snippet';
+$result = get_CURL($urlLatestVideo);
+$latestVideoId = $result['items'][0]['id']['videoId'];
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -42,7 +68,7 @@
       <div class="container">
         <div class="text-center">
           <img src="img/profile1.png" class="rounded-circle img-thumbnail">
-          <h1 class="display-4">Sandhika Galih</h1>
+          <h1 class="display-4">Chaerul Anwar</h1>
           <h3 class="lead">Lecturer | Programmer | Youtuber</h3>
         </div>
       </div>
@@ -69,8 +95,73 @@
     </section>
 
 
+    <!-- Youtube dan instagram -->
+    <section class="social bg-light" id="social">
+      <div class="container">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center">
+            <h2>Social Media</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-5">
+            <div class="row">
+              <div class="col-md-4">
+
+                <img src="<?= $youtubeProfilePic; ?>" width="200" class="rounded-circle img-thumbnail">
+              </div>
+              <div class="col-md-8">
+
+                <h5><?= $chanelName; ?></h5>
+                <p><?= $subscriber; ?></p>
+                <div class="g-ytsubscribe" data-channelid="UCkXmLjEr95LVtGuIm3l2dPg" data-layout="default" data-count="default"></div>
+              </div>
+            </div>
+            <div class="row pt-3 mb-3">
+
+            <div class="col">
+            <div class="embed-responsive embed-responsive-16by9">
+              <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= $latestVideoId;?>?rel=0" allowfullscreen></iframe>
+            </div>
+            </div>
+            </div>
+          </div>
+          <div class="col-md-5">
+            <div class="row">
+              <div class="col-md-4">
+              <img src="img/profile1.png" width="200" class="rounded-circle img-thumbnail">
+              </div>
+              <div class="col-md-8">
+
+              <h5>@chaerulanwarxyz</h5>
+              <p>2.000 Subcriber.</p>
+              </div>
+            </div>
+                <div class="row mt-3 pb-3">
+                  <div class="col">
+                    <div class="ig-thumbnail">
+                      <img src="img/thumbs/1.png">
+                    </div>
+                    <div class="ig-thumbnail">
+                      <img src="img/thumbs/2.png">
+                    </div>
+                    <div class="ig-thumbnail">
+                      <img src="img/thumbs/3.png">
+                    </div>
+                  </div>
+                </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
+
+
     <!-- Portfolio -->
-    <section class="portfolio bg-light" id="portfolio">
+    <section class="portfolio " id="portfolio">
       <div class="container">
         <div class="row pt-4 mb-4">
           <div class="col text-center">
@@ -139,7 +230,7 @@
 
 
     <!-- Contact -->
-    <section class="contact" id="contact">
+    <section class="contact bg-light" id="contact">
       <div class="container">
         <div class="row pt-4 mb-4">
           <div class="col text-center">
@@ -216,5 +307,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <script src="https://apis.google.com/js/platform.js"></script>
   </body>
 </html>
